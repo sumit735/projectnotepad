dcd5341d4887ad7fbb2209023ee8b189b454f27e
sumit <sumitkumardash1999@gmail.com>
2019-04-08 14:11:53 +0530

    password hashing successful

--- index.php
+++ index.php
@@ -36,6 +36,8 @@
                 if(!mysqli_query($connection, $query)) {
                     $error .= "couldn't sign you up.please try again later".mysqli_error($connection);
                 } else {
+                    $query = "UPDATE `users` SET password = '".md5(md5(mysqli_insert_id($connection)).$pass)."' WHERE id = ".mysqli_insert_id($connection)." LIMIT 1";
+                    mysqli_query($connection, $query);
                     echo "signup successful";
                 }
             }
@@ -51,7 +53,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Log In</title>
+    <title>Log In | Sign Up</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
